2be930c828afcd47b3d0704991a0fd9ebc73a762
Serj Zavadsky <fevral13@gmail.com>
2012-08-16 20:22:52 +0300

    media refactored

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 # Django settings for creditreportsite project.
 import os
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
@@ -46,18 +48,18 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -104,8 +106,9 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'creditreportsite.urls'
 
 TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__),'templates').replace('\\','/'),
-    )
+    os.path.join(PROJECT_ROOT, 'templates'),
+    os.path.join(MEDIA_ROOT, 'uploaded_templates'),
+)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
--- website/models.py
+++ website/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from settings_local import MEDIA_ROOT
-from settings import TEMPLATE_DIRS
 
 # Create your models here.
 
@@ -9,17 +7,17 @@ class LandingPage(models.Model):
     header_content = models.CharField(max_length=10)
     body_content = models.CharField(max_length=10)
     lp_file = models.CharField(max_length=100)
-    safe_lp_file = models.FileField(upload_to=TEMPLATE_DIRS[0])
-    force_safe_lp = models.FileField(upload_to=TEMPLATE_DIRS[0], blank=True, null=True)
+    safe_lp_file = models.FileField(upload_to='uploaded_templates')
+    force_safe_lp = models.FileField(upload_to='uploaded_templates', blank=True, null=True)
 
 
 class Keyword(models.Model):
     keyword = models.CharField('bank keyword', max_length=10)
-    image = models.ImageField('bank logo', upload_to=MEDIA_ROOT)
+    image = models.ImageField('bank logo', upload_to='images')
     text = models.CharField('bank text', max_length=20)
     domain = models.CharField('bank domain', max_length=20)
 
     def logo(self):
-        x = '<img src="/media/images/%s"' % self.image.path.split('\\')[-1]
+        x = '<img src="%s" />' % self.image.url
         return x
     logo.allow_tags = True
