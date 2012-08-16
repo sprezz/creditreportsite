5259b59a020c5ee47eda9e5c2659a855274a40a9
sorezz <jacobgriffinmarketing@gmail.com>
2012-08-16 18:23:11 -0400

    urls.py updated

--- website/views.py
+++ website/views.py
@@ -15,6 +15,7 @@ def get_landing_page(request, bank_keyword, lp='lp5'):
     except Keyword.DoesNotExist:
         return HttpResponse('Bank not found in system')
     logo = bank.image.url
+    return render_to_response('%s/safe.html' % lp,locals())
     if v.cloaked:
         return render_to_response('%s/safe.html' % lp,locals())
     return render_to_response('%s/index.html' % lp,locals())
