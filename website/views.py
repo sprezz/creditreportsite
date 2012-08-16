e694ec1a02bf9f98a77fb95e58f0a937304c6324
sorezz <jacobgriffinmarketing@gmail.com>
2012-08-16 14:17:10 -0400

    updated logo in lp

--- website/views.py
+++ website/views.py
@@ -8,5 +8,5 @@ def get_landing_page(request, bank_keyword, lp='lp5'):
         bank = Keyword.objects.get(keyword=bank_keyword)
     except Keyword.DoesNotExist:
         return HttpResponse('Bank not found in system')
-    logo = bank.image.path.split('\\')[-1]
+    logo = bank.image.url
     return render_to_response('%s/index.html' % lp,locals())
