642a887c0078e4f48baa6d3da92fe395aa1ffc05
sorezz <jacobgriffinmarketing@gmail.com>
2012-08-17 18:28:42 -0400

    added search fields

--- website/admin.py
+++ website/admin.py
@@ -4,10 +4,10 @@ from .models import Keyword, LandingPage, Visitor
 
 class KeywordAdmin(admin.ModelAdmin):
     list_display = ('keyword', 'text', 'domain', 'logo')
-    search_fields = ['ip','city','state','country']
 
 class VisitorAdmin(admin.ModelAdmin):
     list_display = ('visit_datetime', 'ip', 'city', 'state', 'country_code', 'keyword', 'lp', 'cloaked', 'reason')
+    search_fields = ['ip','city','state','country']
     ordering = ('-visit_datetime',)
 
 admin.site.register(Visitor, VisitorAdmin)
