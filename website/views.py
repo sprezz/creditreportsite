3a0e998e08aaf36535c6e274250880be673ebaaf
sorezz <jacobgriffinmarketing@gmail.com>
2012-08-17 18:16:05 -0400

    none

--- website/views.py
+++ website/views.py
@@ -20,7 +20,7 @@ def get_landing_page(request, bank_keyword, lp='lp5'):
         v.reason = ''
         v.save()
         return render_to_response('%s/index.html' % lp,locals())
-    return render_to_response('%s/safe.html' % lp,locals())
+#    return render_to_response('%s/safe.html' % lp,locals())
     if v.cloaked:
         return render_to_response('%s/safe.html' % lp,locals())
     return render_to_response('%s/index.html' % lp,locals())
@@ -48,7 +48,7 @@ organizations = ['softlayer',
 cities = [#'dallas',
           'irvine',
           'los angeles']
-states = ['california','ca']
+states = ['california','ca','tx']
 allowed_country = ['us']
 
 def legitimate_visitor(ip, geo_data, v):
