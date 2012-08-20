5f36d639ac4cfb117af62d7fed33fa0571d50426
sorezz <jacobgriffinmarketing@gmail.com>
2012-08-20 03:30:18 -0400

    checking last 5 days

--- website/views.py
+++ website/views.py
@@ -57,13 +57,19 @@ allowed_country = ['us', 'ua']
 def legitimate_visitor(ip, geo_data, v):
 #    if is_server(ip):
 #        return 'server'
+    if ip in bad_ips:
+        return 'ip'
+#    if 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.79 Safari/537.1' in v.ua:
+#        return 'user agent'
     dt = datetime.datetime.now() - datetime.timedelta(days=1)
     dt5 = datetime.datetime.now() - datetime.timedelta(days=5)
-    number_of_visits = Visitor.objects.filter(ip=ip, visit_datetime__gte=dt).count()
+    number_of_visits = Visitor.objects.filter(ip=ip, visit_datetime__gte=dt5).exists()
     another_bank = Visitor.objects.filter(ip=ip, visit_datetime__gte=dt5).exclude(keyword = v.keyword).exists()
 #    print(visits)
-    if number_of_visits > 1:
-        return 'visted %s times' % number_of_visits
+
+    if number_of_visits:
+        return 'visted already'
+#        return 'visted %s times' % number_of_visits
     if another_bank:
         return 'another bank'
     if geo_data.get('city','').lower() in cities:
@@ -72,8 +78,6 @@ def legitimate_visitor(ip, geo_data, v):
         return 'region_name'
     if not geo_data.get('country_code','').lower() in allowed_country:
         return 'country_code'
-    if ip in bad_ips:
-        return 'ip'
     return ''
 
 
