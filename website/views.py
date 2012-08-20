c43844b159a6bdbc631024008d52a8a141965421
sorezz <jacobgriffinmarketing@gmail.com>
2012-08-20 05:38:33 -0400

    referer and ua, saved and dispayed

--- website/views.py
+++ website/views.py
@@ -94,7 +94,7 @@ def save_visitor(request, keyword, lp):
     v.visit_datetime = datetime.datetime.now()
     v.ip = request.META.get('HTTP_X_REAL_IP', '')
     v.ua = request.META['HTTP_USER_AGENT'][:100]
-    v.referer = request.META.get('HTTP_REFERER')
+    v.text = request.META.get('HTTP_REFERER')
     v.keyword = keyword
     v.lp = lp
     v.dt = datetime.datetime.now()
