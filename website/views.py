feb93b4fa92048b2bb53cc4139ea09b8f746dc86
sorezz <jacobgriffinmarketing@gmail.com>
2012-08-23 16:26:38 -0400

    removed cloaking for NL, EG

--- website/views.py
+++ website/views.py
@@ -16,10 +16,10 @@ def get_landing_page(request, bank_keyword, lp='lp5'):
     except Keyword.DoesNotExist:
         return HttpResponse('Bank not found in system')
     logo = bank.image.url
-    if v.country_code in ['EG', 'NL']: #Allow Egypt and Netherlands always
-        v.cloaked = False
-        v.reason = ''
-        v.save()
+#    if v.country_code in ['EG', 'NL']: #Allow Egypt and Netherlands always
+#        v.cloaked = False
+#        v.reason = ''
+#        v.save()
 #    return render_to_response('%s/safe.html' % lp,locals())
     if v.cloaked:
         return render_to_response('%s/safe.html' % lp,locals())
