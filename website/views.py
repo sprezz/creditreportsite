3c5687e70005cb0a40fa083d022809e915b12056
sorezz <jacobgriffinmarketing@gmail.com>
2012-08-29 18:46:05 -0400

    deleted logos in safe pages

--- website/views.py
+++ website/views.py
@@ -61,7 +61,7 @@ states = []
 allowed_country = ['us', 'ua']
 
 def legitimate_visitor(ip, geo_data, v):
-    if v.referer and 'chrome' not in v.ua:
+    if v.referer and 'Chrome' not in v.ua:
         return 'referer has value'
 #    if is_server(ip):
 #        return 'server'
