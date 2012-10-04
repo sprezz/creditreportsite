d0ccf6e18b9b9b57b9d8ee37e9999bd8d76e1e59
SvartalF <self@svartalf.info>
2012-10-04 15:45:32 +0900

    Banned IPs list is accumulated in the one place

--- website/settings.py
+++ website/settings.py
@@ -1,13 +1,15 @@
 BAD_IPS = (  #'208.43.90.178',
     '50.28.69.79',
     '67.227.83.121',
+    '67.227.89.237',
+    '38.81.65.42',
+    '208.43.90.178',
 )
 
 #USE ONLY LOWERCASE
 CITIES = (
     'dallas',
     'irvine',
-    'los angeles'
 )
 
 STATES = (
