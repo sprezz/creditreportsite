33150249eb817c29daa14138f00ffbaac5d38129
SvartalF <self@svartalf.info>
2012-10-05 09:28:37 +0900

    Send visitor to a randomly choosen link from the lp pages

--- website/settings.py
+++ website/settings.py
@@ -1,3 +1,8 @@
+REDIRECT_LINKS = (
+    'http://affiliate.trk4.com/rd/r.php?sid=1028&pub=102743&c1=&c2=&c3=',
+    'http://iluv.clickbooth.com/?E=PsVNmcKJ%2fNRtiqmYmJltKA%3d%3d&s1=',
+)
+
 BAD_IPS = (  #'208.43.90.178',
     '50.28.69.79',
     '67.227.83.121',
