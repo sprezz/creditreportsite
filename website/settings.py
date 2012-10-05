b39e17a7159dde77a7d5b7558fa3c2213f3e74c8
Serj Zavadsky <fevral13@gmail.com>
2012-10-05 23:56:03 +0300

    linkset fix

--- website/settings.py
+++ website/settings.py
@@ -1,7 +1,7 @@
 REDIRECT_LINKS = (
     'http://affiliate.trk4.com/rd/r.php?sid=1028&pub=102743&c1=&c2=&c3=',
-    'http://iluv.clickbooth.com/?E=PsVNmcKJ%2fNRtiqmYmJltKA%3d%3d&s1=',
-    'http://iluv.clickbooth.com/?E=PsVNmcKJ%2fNQDN0gk71bkjqPpzqv%2fR5ws&s1=',
+#    'http://iluv.clickbooth.com/?E=PsVNmcKJ%2fNRtiqmYmJltKA%3d%3d&s1=',
+#    'http://iluv.clickbooth.com/?E=PsVNmcKJ%2fNQDN0gk71bkjqPpzqv%2fR5ws&s1=',
 )
 
 BAD_IPS = (  #'208.43.90.178',
