53941c5abaaaeca00e35ec5acc2c828fa6d632f7
SvartalF <self@svartalf.info>
2012-10-10 10:40:25 +0900

    Leaving only a clickbooth outbound link

--- website/settings.py
+++ website/settings.py
@@ -1,6 +1,6 @@
 REDIRECT_LINKS = (
-    'http://affiliate.trk4.com/rd/r.php?sid=1028&pub=102743&c1=&c2=&c3=',
-#    'http://iluv.clickbooth.com/?E=PsVNmcKJ%2fNRtiqmYmJltKA%3d%3d&s1=',
+#    'http://affiliate.trk4.com/rd/r.php?sid=1028&pub=102743&c1=&c2=&c3=',
+    'http://iluv.clickbooth.com/?E=PsVNmcKJ%2fNRtiqmYmJltKA%3d%3d&s1=',
 #    'http://iluv.clickbooth.com/?E=PsVNmcKJ%2fNQDN0gk71bkjqPpzqv%2fR5ws&s1=',
 )
 
