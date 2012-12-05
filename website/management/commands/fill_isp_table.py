4b25847e179ced9cf96cca722e1a618caab1246c
Serj Zavadsky <fevral13@gmail.com>
2012-12-05 18:05:52 +0200

    fix

--- website/management/commands/fill_isp_table.py
+++ website/management/commands/fill_isp_table.py
@@ -9,9 +9,9 @@ class Command(BaseCommand):
         existing_isps = ISPWhiteList.objects.values_list('name', flat=True)
         print(existing_isps)
 
-        new_isp_name_list = Visitor.objects.distinct().exclude(isp__in=existing_isps).values_list('isp', flat=True)
+        new_isp_name_list = Visitor.objects.distinct().exclude(isp__in=existing_isps, isp__isnull=True).values_list('isp', flat=True)
         print(new_isp_name_list)
 
         for i, name in enumerate(new_isp_name_list):
-            print(name)
+            print(i, name)
             ISPWhiteList.objects.create(name=name)
