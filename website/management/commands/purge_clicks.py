246b355c533dc5c06dd6bba8c2bd528ed94b7fb1
Serj Zavadsky <fevral13@gmail.com>
2013-10-20 17:22:36 +0300

    purge_clicks command

--- website/management/commands/purge_clicks.py
+++ website/management/commands/purge_clicks.py
@@ -1,15 +1,29 @@
 # -*- coding:utf-8 -*-
+import datetime
 from django.core.management import BaseCommand
+from django.db import connection, transaction
 
 from website.models import Visitor
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        visits_with_sales = Visitor.objects.filter(sale=True)
+        if not args:
+            date, count = (datetime.date.today() - datetime.timedelta(days=14)).strftime('%Y-%m-%d'), 50
+        elif len(args) == 1:
+            date, count = args[0], 50
+        else:
+            date, count = args
 
-        with open('ip_sales.csv', 'w') as f:
-            for visit in visits_with_sales:
-#                print('%s,%s' % (visit.ip, visit.visit_datetime.strftime('%Y-%m-%d')))
-                f.write('%s,%s\n' % (visit.ip, visit.visit_datetime.strftime('%Y-%m-%d')))
+        cursor = connection.cursor()
+        cursor.execute("select count(*) from website_visitor where date(visit_datetime) < '%s'" % date)
+        total = cursor.fetchall()[0][0]
+        deleted = 0
+        left = 1
+        while left:
+            left = cursor.execute("delete from website_visitor where date(visit_datetime) < '%s' limit %s" % (date, count))
+            transaction.commit_unless_managed()
+            deleted += left
+            print('deleted %s of %s' % (deleted, total))
+
 
