3b0894091c8aad7de588397bb99e4d388de23f1b
Serj Zavadsky <fevral13@gmail.com>
2013-10-21 00:32:25 +0300

    fix

--- website/templatetags/static_file_prefix.py
+++ website/templatetags/static_file_prefix.py
@@ -10,4 +10,4 @@ def fileurl_prefix(country, landing_page):
     >>> fileurl_prefix('US', 'lp2')
     '/lps/us/lp2'
     """
-    return u'/lps/{}/{}'.format(country.lower(), landing_page.lower())
+    return u'/lps/{0}/{1}'.format(country.lower(), landing_page.lower())
