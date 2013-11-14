7c8322bcd16cb8b8ffdcfe2af8d957b0d4baac74
Serj Zavadsky <fevral13@gmail.com>
2013-11-15 00:17:19 +0200

    subid passing

--- website/views.py
+++ website/views.py
@@ -116,12 +116,12 @@ def unique_subid(request, subid):
     if visitor.cloaked:
         context['redirect_link'] = lp.random_safe_link().url
         if 'subid' in query_dict.keys():
-            context['redirect_link'] += '?subid=%s' % query_dict['subid']
+            context['redirect_link'] += '%s' % query_dict['subid']
         return render_to_response('landing_pages/{0}/{1}/safe.html'.format(keyword_country.lower(), visitor.lp), context)
     else:
         context['redirect_link'] = lp.random_index_link().url
         if 'subid' in query_dict.keys():
-            context['redirect_link'] += '?subid=%s' % query_dict['subid']
+            context['redirect_link'] += '%s' % query_dict['subid']
         return render_to_response('landing_pages/{0}/{1}/index.html'.format(keyword_country.lower(), visitor.lp), context)
 
 
